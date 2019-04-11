cd69b873964403894700e0353caebe0ba6837765
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-11 14:33:25 +0200

    Task repository added

---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/model/task/TaskRepository.java
@@ -0,0 +1,6 @@
+package cz.ppro.taskmanager.taskamanager.model.task;
+
+import org.springframework.data.repository.Repository;
+
+public interface TaskRepository extends Repository<Task, Integer> {
+}
