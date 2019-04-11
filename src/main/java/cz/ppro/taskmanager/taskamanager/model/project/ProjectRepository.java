3e24230c8ba10cf253086f38bd05f7defa98cca9
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-11 14:34:05 +0200

    Project repository added

---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/model/project/ProjectRepository.java
@@ -0,0 +1,6 @@
+package cz.ppro.taskmanager.taskamanager.model.project;
+
+import org.springframework.data.repository.Repository;
+
+public interface ProjectRepository extends Repository<Project, Integer> {
+}
