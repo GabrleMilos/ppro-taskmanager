38d7c7df8d45c88592c06db1821ac3bb52dcc105
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-11 14:32:54 +0200

    TaskHistory repository added

---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/model/task_history/TaskHistoryRepository.java
@@ -0,0 +1,6 @@
+package cz.ppro.taskmanager.taskamanager.model.task_history;
+
+import org.springframework.data.repository.Repository;
+
+public interface TaskHistoryRepository extends Repository<TaskHistory, Integer> {
+}
