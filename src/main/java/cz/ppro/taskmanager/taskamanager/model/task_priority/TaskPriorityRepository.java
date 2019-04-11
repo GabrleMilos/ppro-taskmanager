70c745ffee9233d562a641c56b8559720fe03190
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-11 14:32:18 +0200

    TaskPriority repository added

---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/model/task_priority/TaskPriorityRepository.java
@@ -0,0 +1,6 @@
+package cz.ppro.taskmanager.taskamanager.model.task_priority;
+
+import org.springframework.data.repository.Repository;
+
+public interface TaskPriorityRepository extends Repository<TaskPriority, Integer> {
+}
