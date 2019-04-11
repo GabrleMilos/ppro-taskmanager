6090d69121fdfcc96480b2a9f802783aac3678e3
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-11 14:31:34 +0200

    TaskState and TaskType repository added

---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/model/task_state/TaskStateRepository.java
@@ -0,0 +1,6 @@
+package cz.ppro.taskmanager.taskamanager.model.task_state;
+
+import org.springframework.data.repository.Repository;
+
+public interface TaskStateRepository extends Repository<TaskState, Integer> {
+}
---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/model/task_type/TaskTypeRepository.java
@@ -0,0 +1,7 @@
+package cz.ppro.taskmanager.taskamanager.model.task_type;
+
+import org.springframework.data.repository.Repository;
+
+public interface TaskTypeRepository extends Repository<TaskType, Integer> {
+
+}
