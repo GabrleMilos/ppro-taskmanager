7936f23fd6a6176a5f9511cd18e9e40f5bc42673
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-11 16:15:01 +0200

    project controller added

---
+++ src/main/java/cz/ppro/taskmanager/taskamanager/controller/ProjectController.java
@@ -0,0 +1,25 @@
+package cz.ppro.taskmanager.taskamanager.controller;
+
+import cz.ppro.taskmanager.taskamanager.model.project.Project;
+import cz.ppro.taskmanager.taskamanager.model.project.ProjectRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProjectController {
+    private final ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @RequestMapping("/project/myProjects/{userId}")
+    public List<Project> projects (@PathVariable String userId){
+        return null;
+    }
+
+
+}
--- src/main/java/cz/ppro/taskmanager/taskamanager/controller/UserController.java
+++ src/main/java/cz/ppro/taskmanager/taskamanager/controller/UserController.java
@@ -2,7 +2,6 @@ package cz.ppro.taskmanager.taskamanager.controller;
 
 import cz.ppro.taskmanager.taskamanager.model.user.User;
 import cz.ppro.taskmanager.taskamanager.model.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
@@ -12,12 +11,11 @@ public class UserController {
 
     private final UserRepository userRepository;
 
-    @Autowired
     UserController(UserRepository userRepository){
         this.userRepository =  userRepository;
     }
 
-    @RequestMapping("/findUsers")
+    @RequestMapping("/users/findUsers")
     public Collection<User> getUsers(){
 
         Collection col = userRepository.findAll();
