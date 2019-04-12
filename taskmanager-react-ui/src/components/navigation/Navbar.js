6996c7938e4cf771b5c7e41e1ebf0b50f99084be
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-12 20:14:23 +0200

    conditions for navbar

--- taskmanager-react-ui/src/components/navigation/Navbar.js
+++ taskmanager-react-ui/src/components/navigation/Navbar.js
@@ -3,13 +3,15 @@ import {Link} from "react-router-dom";
 import LoggedInLinks from './LoggedInLinks'
 import LoggedOffLinks from './LoggedOffLinks'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    //TODO: dodelat autentizaci z controlleru
+    const isLoggedIn = true;
+    const links = isLoggedIn? <LoggedInLinks/> : <LoggedOffLinks/>;
     return (
         <nav className="grey darken-3">
             <div className="container nav-wrapper">
                 <Link to='/' className='brand-logo'> TaskManager </Link>
-                <LoggedInLinks/>
-                <LoggedOffLinks/>
+                {links}
             </div>
         </nav>
     )
