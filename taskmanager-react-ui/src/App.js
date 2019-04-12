b3b70024cef7d83287fd7a21951413acca1be1af
gabrlemilos@gmail.com <gabrlemilos@gmail.com>
2019-04-12 14:34:19 +0200

    updated files

--- taskmanager-react-ui/src/App.js
+++ taskmanager-react-ui/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {Login} from './pages/Login';
-import {Error} from "./pages/Error";
-import {Navigation} from "./Navigation";
+import {Login} from './components/authorization/Login';
+import {Error} from "./components/Error";
+import {Navigation} from "./components/navigation/Navigation";
 import './App.css';
 
 class App extends Component {
