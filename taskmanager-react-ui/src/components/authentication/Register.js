a36f2f42711e43ec11dd19fc1967dc20bb3969a1
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-16 15:47:11 +0200

    added components

---
+++ taskmanager-react-ui/src/components/authentication/Register.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+export class Register extends Component {
+    state = {
+        email:'',
+        password:'',
+        fistName:'',
+        lastName:''
+    };
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmit} className='white'>
+                    <h5 className="grey-text text-darken-3">Sign in</h5>
+                    <div className="input-field">
+                        <label htmlFor='email'>Email</label>
+                        <input id='email' type='email' onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor='password'>Password</label>
+                        <input id='password' type='password' onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor='firstName'>First name</label>
+                        <input id='firstName' type='text' onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor='lastName'>Last Name</label>
+                        <input id='lastName' type='text' onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <button className="btn pink lighten-1 z-depth-0">Register</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
---
+++ taskmanager-react-ui/src/components/navigation/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import LoggedInLinks from './LoggedInLinks'
+import LoggedOffLinks from './LoggedOffLinks'
+
+const Navbar = () => {
+    const isLoggedIn = true;
+    const links = isLoggedIn? <LoggedInLinks/> : <LoggedOffLinks/>;
+    return (
+        <nav className="grey darken-3">
+            <div className="container nav-wrapper">
+                <Link to='/' className='brand-logo'> TaskManager </Link>
+                {links}
+            </div>
+        </nav>
+    )
+}
+export default Navbar
