32012078da03d0915a648ab5d569115a0e4040c2
gabrlmi1 <gabrlemilos@gmail.com>
2019-05-01 12:56:18 +0200

    get tasksForUser

--- src/main/java/cz/ppro/taskmanager/taskamanager/controller/TaskController.java
+++ src/main/java/cz/ppro/taskmanager/taskamanager/controller/TaskController.java
@@ -1,7 +1,8 @@
 package cz.ppro.taskmanager.taskamanager.controller;
 
-import cz.ppro.taskmanager.taskamanager.model.project.Project;
+import cz.ppro.taskmanager.taskamanager.model.task.Task;
 import cz.ppro.taskmanager.taskamanager.model.task.TaskRepository;
+import cz.ppro.taskmanager.taskamanager.model.user.UserRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,20 @@ import java.util.List;
 @RestController
 public class TaskController {
     private TaskRepository taskRepository;
+    private UserRepository userRepository;
 
-    public TaskController(TaskRepository taskRepository) {
+    public TaskController(TaskRepository taskRepository, UserRepository userRepository) {
         this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
     }
 
-    @RequestMapping("/task/tasksForProject/{userId}/{projectId}")
-    public List<Project> projects (@PathVariable Integer userId,@PathVariable Integer projectId){
+    @RequestMapping("/task/userTasksForProject/{userId}/{projectId}")
+    public List<Task> getUserTasksForProject (@PathVariable Integer userId,@PathVariable Integer projectId){
         return null;
+    }
+
+    @RequestMapping("/task/userTasks/{email}")
+    public List<Task> getUserTasks (@PathVariable String email){
+        return taskRepository.findAllByAssignedUserOrderByCreated(userRepository.findByEmail(email));
     }
 }
