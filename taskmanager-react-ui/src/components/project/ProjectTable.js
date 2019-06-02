9dcd68eb832361575b89f3057ee407cf4c297b22
gabrlemilos@gmail.com <gabrlemilos@gmail.com>
2019-06-02 11:21:16 +0200

    new project button

--- taskmanager-react-ui/src/components/project/ProjectTable.js
+++ taskmanager-react-ui/src/components/project/ProjectTable.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import M from "materialize-css";
+import {NavLink} from "react-router-dom";
 
 
 export class ProjectTable extends Component {
@@ -10,28 +11,30 @@ export class ProjectTable extends Component {
     }
 
     render() {
-            const {projects} = this.props;
-            console.log(projects);
+        const {projects} = this.props;
         return (
-            <table className='striped'>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Created</th>
-                    <th>Project manager</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                {projects && projects.map(project =>
-                    <tr key={project.id}>
-                        <td>{project.name}</td>
-                        <td>{project.created}</td>
-                        <td>{project.manager.email}</td>
+            <div>
+                <NavLink to='/project/new' className='btn pink lighten-1 z-depth-0'> New Project </NavLink>
+                <table className='striped'>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Created</th>
+                        <th>Project manager</th>
                     </tr>
-                )}
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody>
+                    {projects && projects.map(project =>
+                        <tr key={project.id}>
+                            <td>{project.name}</td>
+                            <td>{project.created}</td>
+                            <td>{project.manager.email}</td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
